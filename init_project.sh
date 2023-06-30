b29bbafe20c10fb3b0170a0f86f501f50d1a5f4c
TannerB <tannerpace@gmail.com>
2023-06-30 14:16:41 -0400

    implement refresh_token

--- init_project.sh
+++ init_project.sh
@@ -273,6 +273,8 @@ EOF
 
 # Adding  authentication Middlewares
 cat << EOF > src/middleware/authentication.ts
+import { randomBytes } from 'crypto';
+
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
@@ -390,6 +392,19 @@ const updateLastLogin = async (userId: number): Promise<void> => {
     });
 };
 
+const createRefreshToken = async (userId: number): Promise<string> => {
+  const refreshToken = randomBytes(64).toString('hex');
+
+  await prisma.user.update({
+    where: { id: userId },
+    data: { refresh_token: refreshToken },
+  });
+
+  return refreshToken;
+};
+
+
+
 const handleError = (err: Error, res: Response): Response => {
   console.error(err);
   if (err.message === 'Token has expired') {
@@ -403,8 +418,10 @@ export {
   checkUserToken,
   verifyToken,
   getUserFromToken,
-  handleError
+  handleError,
+  createRefreshToken
 };
+
 EOF
 
 
@@ -413,13 +430,10 @@ EOF
 # Creating a user router
 cat << EOF > src/routes/userRouter.ts
 
-
-
-
 import express, { Router, Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-import { updateLastLogin } from '../middleware/authentication';
+import { createRefreshToken, updateLastLogin } from '../middleware/authentication';
 import prisma from "../middleware/client";
 import { hashPassword, verifyPassword } from '../middleware/hash';
 
@@ -431,70 +445,13 @@ interface ParsedToken {
   exp: number;
 }
 
-
-
-// Create an instance of the router
 const userRouter: Router = express.Router();
 
-// Middleware function to verify the token
-const verifyToken = (req: any, res: Response, next: NextFunction) => {
-  console.info("hit verify")
-  const token = req.cookies.token; // Access the token from the "token" cookie
-  console.info("token was ", token)
-
-  if (!token) {
-    return res.status(401).json({ error: 'Token not found' });
-  }
-
-  const secret = process.env.JWT_SECRET;
-  if (!secret) {
-    throw new Error('JWT secret is not defined');
-  }
-
-  try {
-    const parsedToken = jwt.verify(token, secret) as ParsedToken;
-    req.parsedToken = parsedToken; // Store the parsed token in the request object for later use
-    next();
-  } catch (err) {
-    // if (err.name === 'TokenExpiredError') {
-    //   return res.status(401).json({ error: 'Token has expired' });
-    // }
-    return res.status(500).json({ error: 'Error validating token' });
-  }
-};
-
-
-// Example protected route handler
-const someProtectedRouteHandler = (req: any, res: Response) => {
-  // Access the parsedToken from the request object
-  const parsedToken = req.parsedToken;
-
-  // Access the user ID from the parsedToken
-  const userId = parsedToken?.userData.id;
-
-  // Perform actions specific to the protected route
-  // ...
-
-  // Send a response
-  res.json({ message: 'Protected route accessed successfully', userId });
-};
-
-// Define the protected route
-userRouter.get('/verify', verifyToken, someProtectedRouteHandler);
-
-
-
-// GET /user
-userRouter.get('/', (req, res) => {
+const sayHelloUser = (req: Request, res: Response) => {
   res.json({ message: 'Hello user' });
-});
+};
 
-
-
-
-
-// POST /user (user registration)
-userRouter.post('/', async (req, res) => {
+const registerUser = async (req: Request, res: Response) => {
   const { firstName, lastName, email, password } = req.body;
   try {
     const { salt, hashedPassword } = hashPassword(password);
@@ -518,9 +475,55 @@ userRouter.post('/', async (req, res) => {
   } catch (error) {
     res.json({ message: 'User not created' });
   }
-});
+};
 
-userRouter.post('/login', async (req: Request, res: Response) => {
+const refreshToken = async (req: Request, res: Response) => {
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    return res.status(401).json({ error: 'Refresh token not found' });
+  }
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        refresh_token: refreshToken,
+      },
+      select: { id: true },
+    });
+
+    if (user) {
+      const secret = process.env.JWT_SECRET;
+      if (!secret) {
+        throw new Error('JWT secret is not defined');
+      }
+
+      // Generate a new JWT token
+      const token = jwt.sign({ userData: { id: user.id, timestamp: new Date().getTime() } }, secret, { expiresIn: '1h' });
+
+      // Generate a new refresh token and update it in the database
+      const newRefreshToken = await createRefreshToken(user.id);
+
+      // Update the cookies with the new tokens
+      res
+        .cookie('token', token, { httpOnly: true, maxAge: 3600000 })
+        .cookie('refreshToken', newRefreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        .json({ message: 'New token issued', token });
+    } else {
+      console.info('Invalid refresh token');
+      res.status(401).json({ message: 'Invalid refresh token' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong', error });
+  }
+};
+
+// Add the refresh token route
+userRouter.post('/refresh_token', refreshToken);
+
+
+const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
     const user = await prisma.user.findUnique({
@@ -531,21 +534,18 @@ userRouter.post('/login', async (req: Request, res: Response) => {
     });
     if (user && user.salt && user.password) {
       if (verifyPassword(password, user.password, user.salt)) {
-        console.info('User logged in');
-
-        // Generate a JWT for the user
         const secret = process.env.JWT_SECRET;
         if (!secret) {
           throw new Error('JWT secret is not defined');
         }
-        const token = jwt.sign({ userData: { id: user.id } }, secret, { expiresIn: '1h' });
-
-        // update last login
+        const token = jwt.sign({ userData: { id: user.id, timestamp: new Date().getTime() } }, secret, { expiresIn: '1h' });
+        const refreshToken = await createRefreshToken(user.id);
         await updateLastLogin(user.id);
 
         res
           .cookie('token', token, { httpOnly: true, maxAge: 3600000 }) // Set the token as an HTTP-only cookie
-          .json({ message: 'User logged in', token });
+          .cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 }) // Set the refreshToken as an HTTP-only cookie
+          .json({ message: 'User logged in', token, refreshToken });
       } else {
         console.info('Wrong password');
         res.status(401).json({ message: 'Wrong password' });
@@ -558,12 +558,54 @@ userRouter.post('/login', async (req: Request, res: Response) => {
     console.error(error);
     res.status(500).json({ message: 'Something went wrong', error });
   }
-});
+};
+
+
+// Middleware function to verify the token
+const verifyToken = (req: any, res: Response, next: NextFunction) => {
+  const token = req.cookies.token; // Access the token from the "token" cookie
+  console.info("token was ", token)
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token not found' });
+  }
+
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    throw new Error('JWT secret is not defined');
+  }
+
+  try {
+    const parsedToken = jwt.verify(token, secret) as ParsedToken;
+    req.parsedToken = parsedToken; // Store the parsed token in the request object for later use
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid or expired token' });
+  }
+};
+
+const someProtectedRouteHandler = (req: any, res: Response) => {
+  // Access the parsedToken from the request object
+  const parsedToken = req.parsedToken;
+
+  // Access the user ID from the parsedToken
+  const userId = parsedToken?.userData.id;
+
+  // Perform actions specific to the protected route
+  // ...
+
+  // Send a response
+  res.json({ message: 'Protected route accessed successfully', userId });
+};
+
+// Define the routes
+userRouter.get('/', sayHelloUser);
+userRouter.get('/safeHello', verifyToken, sayHelloUser)
+userRouter.post('/', registerUser);
+userRouter.post('/login', loginUser);
+userRouter.get('/verify', verifyToken, someProtectedRouteHandler);
 
 export default userRouter;
-
-
-
 
 EOF
 
